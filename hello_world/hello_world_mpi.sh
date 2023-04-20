14a9addbfa4c9cfe5d67793eff5b249503ffc0f2
jameshu15869 <jhudson15869@gmail.com>
2023-04-20 00:24:13 -0400

    edit hello world mpi batch script

--- hello_world/hello_world_mpi.sh
+++ hello_world/hello_world_mpi.sh
@@ -3,6 +3,14 @@
 # SLURM information
 #SBATCH -t 00:05:00
 #SBATCH -n 4
-#SBATCH --mem=5g
+#SBATCH --mem=4g
 #SBATCH -p batch
 #SBATCH -J HelloWorldMPIJob
+
+#SBATCH -o HelloWorldMPI-%j.out
+#SBATCH -e HelloWorldMPI-%j.err
+
+module load mpi
+
+make hello_world_mpi
+srun --mpi=pmix ./hello_world_mpi
--- hello_world/hello_world_serial.sh
+++ hello_world/hello_world_serial.sh
@@ -5,6 +5,7 @@
 #SBATCH --mem=4g
 #SBATCH -p batch
 #SBATCH -J HelloWorldSerial
+#SBATCH -o hello_world_serial-%j.out
 
 make hello_world_serial
 ./hello_world_serial
